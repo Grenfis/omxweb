f4438144cb00af29bfbf1c2a8d41574bb5bb0d6c
Viktor <zzz@gmail.com>
2019-01-13 13:47:37 +1100

    Перевеле строку меню в компонент

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,43 @@
+var gmenu = {
+    template: `
+    <nav class="navbar navbar-expand-xs navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">{{info.os}}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    `,
+    data: function() {
+        return {
+            info: ''
+        }
+    },
+    created: function() {
+        fetch('/host')
+        .then((response) => {
+            if(response.ok) {
+                return response.json()
+            }
+            throw new Error('Can\'t get host info')
+        }).then((json) => {
+            this.info = json
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
 var brwsr =  {
     template: `
     <div class="col-12">
@@ -142,7 +182,8 @@ var app = new Vue({
     },
     components: {
         "c_browser": brwsr,
-        "c_controller": cntrlr
+        "c_controller": cntrlr,
+        "c_gmenu": gmenu
     },
     methods: {
         get_status: function() {
